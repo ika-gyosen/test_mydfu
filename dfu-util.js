92f1d243f21c6955c6177178f85dce71275187ff
ss4223 <>
2019-03-17 21:47:50 +0900

    3

--- dfu-util.js
+++ dfu-util.js
@@ -493,12 +493,12 @@ var device = null;
                 device.close().then(onDisconnect);
                 device = null;
             } else {
-                // let filters = [];
-                // if (serial) {
-                //     filters.push({ 'serialNumber': serial });
-                // } else if (vid) {
-                //     filters.push({ 'vendorId': vid });
-                // }
+                let filters = [];
+                if (serial) {
+                    filters.push({ 'serialNumber': serial });
+                } else if (vid) {
+                    filters.push({ 'vendorId': vid });
+                }
                 navigator.usb.requestDevice({ /*'filters': filters*/ }).then(
                     async selectedDevice => {
                         let interfaces = dfu.findDeviceDfuInterfaces(selectedDevice);
