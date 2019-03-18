6c21a7ead27de82985512a722168398d6bf657f7
ss4223 <>
2019-03-18 22:18:56 +0900

    filter coment out

--- dfu-util.js
+++ dfu-util.js
@@ -507,7 +507,7 @@ var device = null;
                 } else if (vid) {
                     filters.push({ 'vendorId': vid });
                 }
-                filters = [{vendorId: 0x0483 , productId: 0xdf11}];
+                //filters = [{vendorId: 0x0483 , productId: 0xdf11}];
                 navigator.usb.requestDevice({filters: filters}).then(
                     async selectedDevice => {
                         let interfaces = dfu.findDeviceDfuInterfaces(selectedDevice);
